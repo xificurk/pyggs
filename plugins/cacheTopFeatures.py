10a8cfc85e244735de151e2803d05caa5bd8f423
Petr Morávek [Xificurk] <xificurk@centrum.cz>
2009-09-08 23:14:09 +0200

    Better sorting

--- plugins/cacheTopFeatures.py
+++ plugins/cacheTopFeatures.py
@@ -47,8 +47,11 @@ class cacheTopFeatures(object):
         """Run the plugin's code"""
         self.log.info("Running...")
 
-        myFinds = self.master.plugins["myFinds"].storage.getList()
-        caches  = self.master.plugins["cache"].storage.select(myFinds)
+        myFinds = self.master.plugins["myFinds"].storage.select("SELECT * FROM myFinds")
+        myFinds = self.master.profileStorage.fetchAssoc(myFinds, "guid")
+        caches  = self.master.plugins["cache"].storage.select(self.master.plugins["myFinds"].storage.getList())
+        for cache in caches:
+            cache.update(myFinds[cache["guid"]])
 
         self.templateData["distances"]  = self.getTopDistances(caches)
         self.templateData["directions"] = self.getTopDirections(caches)
@@ -61,9 +64,9 @@ class cacheTopFeatures(object):
         distances = {"min":caches[0], "max":caches[0]}
         for cache in caches:
             cache["distance"] = self.master.plugins["cache"].distance(cache["lat"], cache["lon"])
-            if cache["distance"] > distances["max"]["distance"]:
+            if cache["distance"] > distances["max"]["distance"] or (cache["distance"] == distances["max"]["distance"] and cache["date"] < distances["max"]["date"]):
                 distances["max"] = cache
-            if cache["distance"] < distances["min"]["distance"]:
+            if cache["distance"] < distances["min"]["distance"] or (cache["distance"] == distances["min"]["distance"] and cache["date"] < distances["min"]["date"]):
                 distances["min"] = cache
 
         return distances
@@ -72,13 +75,13 @@ class cacheTopFeatures(object):
     def getTopDirections(self, caches):
         directions = {"north":caches[0], "south":caches[0],"east":caches[0], "west":caches[0]}
         for cache in caches:
-            if cache["lat"] > directions["north"]["lat"]:
+            if cache["lat"] > directions["north"]["lat"] or (cache["lat"] == directions["north"]["lat"] and cache["date"] < directions["north"]["date"]):
                 directions["north"] = cache
-            if cache["lat"] < directions["south"]["lat"]:
+            if cache["lat"] < directions["south"]["lat"] or (cache["lat"] == directions["south"]["lat"] and cache["date"] < directions["south"]["date"]):
                 directions["south"] = cache
-            if cache["lon"] > directions["east"]["lon"]:
+            if cache["lon"] > directions["east"]["lon"] or (cache["lon"] == directions["east"]["lon"] and cache["date"] < directions["east"]["date"]):
                 directions["east"] = cache
-            if cache["lon"] < directions["west"]["lon"]:
+            if cache["lon"] < directions["west"]["lon"] or (cache["lon"] == directions["west"]["lon"] and cache["date"] < directions["west"]["date"]):
                 directions["west"] = cache
 
         return directions
@@ -87,9 +90,9 @@ class cacheTopFeatures(object):
     def getTopAge(self, caches):
         age = {"min":caches[0], "max":caches[0]}
         for cache in caches:
-            if cache["hidden"] > age["max"]["hidden"]:
+            if cache["hidden"] > age["max"]["hidden"] or (cache["hidden"] == age["max"]["hidden"] and cache["date"] < age["max"]["date"]):
                 age["max"] = cache
-            if cache["hidden"] < age["min"]["hidden"]:
+            if cache["hidden"] < age["min"]["hidden"] or (cache["hidden"] == age["min"]["hidden"] and cache["date"] < age["min"]["date"]):
                 age["min"] = cache
 
         return age

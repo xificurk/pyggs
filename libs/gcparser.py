fb0fc8f643bc87cf0e01196fa285ee2f967a29bf
Petr Morávek [Xificurk] <xificurk@gmail.com>
2011-06-08 15:23:37 +0200

    Correct license statements.

--- libs/gcparser.py
+++ libs/gcparser.py
@@ -25,7 +25,7 @@ Classes:
 
 __author__ = "Petr Morávek (xificurk@gmail.com)"
 __copyright__ = "Copyright (C) 2009-2011 Petr Morávek"
-__license__ = "GPL"
+__license__ = "LGPL 3.0"
 
 __version__ = "0.7.4"
 
